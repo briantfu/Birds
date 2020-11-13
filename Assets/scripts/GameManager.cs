26631f357d4cc1467839ae39d0fa29ad9bdace8c
Brian Fu <briantfu@yahoo.com>
2020-11-13 17:23:33 -0600

    Add files via upload

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject scoreCanvas;
+    public GameObject gameOverCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;    
+    }
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        scoreCanvas.SetActive(false);
+        gameOverCanvas.SetActive(true);
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/RepositionGround.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEditor.Timeline;
+using UnityEngine;
+
+public class RepositionGround : MonoBehaviour
+{
+    BoxCollider2D box;
+    float totallength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        box = GetComponent<BoxCollider2D>();
+        totallength = box.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x <= -totallength)
+        {
+            transform.position = (Vector2)transform.position + Vector2.right * totallength * 2f;
+        }
+    }
+}
